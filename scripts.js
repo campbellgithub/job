780a8d685a3d7000a5364b6f8d9cd7b1742df377
campbellgithub <141380464+campbellgithub@users.noreply.github.com>
2023-10-24 14:03:35 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,8 @@ function clearText() {
 function submitText() {
     const template = `Please help me craft a three-paragraph cover letter for a financial analyst position. In the first paragraph, I want to introduce myself, state what I'm seeking, and briefly touch on my core beliefs and values. In the second paragraph, I'd like to transition into discussing my qualifications and skills, aligning them with the job description. In the third paragraph, I want to explain why I am specifically interested in working for this company and conclude the letter effectively. Let's make this cover letter polished and reflective of Harvard-level professionalism.
 
-I Want you to base it on the following job description and resume:    Resume: 
+I Want you to base it on the following job description and resume:    
+Resume: 
                                                   Kellan Campbell
 432 West Gorham St. | Madison, WI 53703 | kdcampbell2@wisc.edu | (608) 515-3570
 
@@ -46,8 +47,7 @@ Customer Service                                
 · Responded to customer inquiries and resolved customer complaints in a professional and prompt manner, utilizing strong communication and interpersonal skills
 · Assisted customers with services such as event bookings, memberships, and Union event information
 
-Job Description: 
-
+Job Description:
 ${document.getElementById("jobDescription").value}`;
     document.getElementById("output").innerText = template;
 }
